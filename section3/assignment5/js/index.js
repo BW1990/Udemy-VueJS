2a3136f96429c22f4bf6a5ccc5719818ebad7a72
Brian <brianwinward@hotmail.com>
2017-09-17 17:20:26 -0400

    MonsterSlayer game

---
+++ section3/assignment5/js/index.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+    show: true,
+    elements: ["Bob", "Rob", "Bobert"],
+    author: {title: "Lord of the Rings", author: "J.R.R. Tolkien", books: "3"},
+    final: {name: "TESTOBJECT", data: [1.67, 3.33, 0.98, 2.21]}
+  },
+});
