381c05bfbadc0e8415a2301754aba6f2e11286a9
Brian <brianwinward@hotmail.com>
2017-09-19 15:06:29 -0400

    Component Intro

---
+++ assignment8/src/components/Server/Server.vue
@@ -0,0 +1,28 @@
+<template>
+
+          <li
+            class="list-group-item"
+            style="cursor: pointer"
+            @click="serverSeleted"
+          >
+              Server #{{ server.id }}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+  import { serverBus } from "../../main"
+  
+  export default {
+    props: ["server"],
+    methods: {
+      serverSeleted() {
+        serverBus.$emit("serverSelected", this.server);
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
