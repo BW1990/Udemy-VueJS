b22d98c89a0fda5357b17bcd992f2616e34e9597
Brian <brianwinward@hotmail.com>
2017-09-27 12:27:29 -0400

    section 16

---
+++ section16/src/components/user/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3>Some User Details</h3>
+    <p>Loaded ID: {{ $route.params.id }}</p>
+    <hr>
+    <router-link
+      tag="button"
+      :to="link"
+      class="btn btn-primary"
+    >Edit User</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: { name: 'userEdit',
+              params: { id: this.$route.params.id },
+              query: { locale: 'en' },
+              hash: '#data'
+            }
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('in component beforeRouteEnter');
+    if (true) {
+      next();
+    } else {
+      next(false);
+    }
+  }
+}
+</script>
